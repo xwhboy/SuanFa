ca9cf6b67bc4200cd9a71f0fced20885a2a6b2af
xwh12 <xwh1241361935@qq.com>
2016-08-11 02:02:03 +0800

    c2

---
+++ SF/src/searchMatrix_solution.java
@@ -0,0 +1,37 @@
+/**
+ * 写出一个高效的算法来搜索 m × n矩阵中的值。
+
+ 这个矩阵具有以下特性：
+
+ 每行中的整数从左到右是排序的。
+ 每行的第一个数大于上一行的最后一个整数。
+ [
+ [1, 3, 5, 7],
+ [10, 11, 16, 20],
+ [23, 30, 34, 50]
+ ]
+ */
+
+
+/**
+ * Created by xwh12 on 2016/8/10.
+ */
+
+
+public class searchMatrix_solution {
+
+        /**
+         * @param matrix, a list of lists of integers
+         * @param target, an integer
+         * @return a boolean, indicate whether matrix contains target
+         */
+        public boolean searchMatrix(int[][] matrix, int target) {
+            // write your code here
+            int length = matrix[0].length;
+            System.out.println("dfdf");
+            return false;
+        }
+
+
+
+}
